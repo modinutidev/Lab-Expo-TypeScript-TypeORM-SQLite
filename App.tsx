633879a2f89deca04828b595112b070b79e0b69f
Bruno Modinuti <modinutidev@gmail.com>
2022-12-08 20:10:48 -0300

    Adiciona novos componentes para testar funcionalidades

--- App.tsx
+++ App.tsx
@@ -1,44 +1,89 @@
-import { FlatList, StatusBar, Text, View } from "react-native";
+import {
+  FlatList,
+  StatusBar,
+  Text,
+  View,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 
 import "reflect-metadata";
-import { connect } from "./dbConnection";
-import { useEffect, useState } from "react";
+import { initialize as dbSQLite } from "./SQLiteConnect";
+import { useEffect, useState, ChangeEvent } from "react";
 
 export default function App() {
   const [users, setUsers] = useState([]);
-
-  async function connection() {
-    await connect
-      .then(async (db) => {
-        console.log("Banco conectado!");
-
-        // Adiciona um usuário na tabela
-        await db.getRepository("user").save({
-          name: "Nome Completo do Usuários",
-          idade: 21,
-        });
-
-        // Busca todos usuários cadastrados
-        setUsers(await db.getRepository("user").find());
-
-        // Monstra usuários no console
-        console.log(users);
-      })
-      .catch((error) => console.error(error));
-  }
+  const [nome, setNome] = useState("");
 
   useEffect(() => {
-    connection();
+    // connectionTest();
   }, []);
+
+  // async function connectionTest() {
+  //   // Conectar banco de dados local
+  //   const db = await dbSQLite;
+
+  //   // Insere registro
+  //   await db
+  //     .getRepository("usuarios")
+  //     .save({ nome: "Nome Completo do Usuário", idade: 20 });
+
+  //   // Busca e armazena informações do banco em uma useState
+  //   setUsers(await db.getRepository("usuarios").find());
+  // }
+
+  async function insertUser() {
+    const db = await dbSQLite;
+    await db.getRepository("usuarios").save({ idade: 0, nome: nome });
+  }
+
+  async function selectUsers() {
+    const db = await dbSQLite;
+    setUsers(await db.getRepository("usuarios").find());
+  }
+
+  async function actionButton() {
+    await insertUser();
+    await selectUsers();
+    setNome("");
+  }
 
   return (
     <View>
       <StatusBar />
+
+      {/* INPUT */}
+      <View style={{ padding: 10 }}>
+        <TextInput
+          value={nome}
+          style={styles.input}
+          placeholder="Digite um nome..."
+          onChangeText={(value) => setNome(value)}
+        />
+        <TouchableOpacity style={styles.button} onPress={actionButton}>
+          <Text style={styles.textButton}>Adicionar</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* OUTPUT */}
       <FlatList
+        style={{ padding: 10 }}
         data={users}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Text>{item.name}</Text>}
+        renderItem={({ item }) => <Text>{item.nome}</Text>}
       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  input: { height: 40, backgroundColor: "#ddd", paddingHorizontal: 10 },
+  button: {
+    height: 40,
+    backgroundColor: "green",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textButton: { color: "white", fontWeight: "bold" },
+});
