c61f7127643451381ee7d8a8f50a9cffd8bd07bd
Bruno Modinuti <modinutidev@gmail.com>
2022-12-08 20:09:38 -0300

    Atualizado entidade usuário

--- Usuarios.tsx
+++ Usuarios.tsx
@@ -1,12 +1,12 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
-export class User {
+export class Usuarios {
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column("varchar")
-  name: string;
+  nome: string;
 
   @Column("integer")
   idade: number;
