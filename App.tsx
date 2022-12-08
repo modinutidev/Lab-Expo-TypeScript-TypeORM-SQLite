79730b335a4c8abb9cbaac054531125056aab57c
Bruno Modinuti <modinutidev@gmail.com>
2022-12-08 00:56:43 -0300

    Adicionado testes

--- App.tsx
+++ App.tsx
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StatusBar, Text, View } from "react-native";
+
+import "reflect-metadata";
+import { connect } from "./dbConnection";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [users, setUsers] = useState([]);
+
+  async function connection() {
+    await connect
+      .then(async (db) => {
+        console.log("Banco conectado!");
+
+        // Adiciona um usuário na tabela
+        await db.getRepository("user").save({
+          name: "Nome Completo do Usuários",
+          idade: 21,
+        });
+
+        // Busca todos usuários cadastrados
+        setUsers(await db.getRepository("user").find());
+
+        // Monstra usuários no console
+        console.log(users);
+      })
+      .catch((error) => console.error(error));
+  }
+
+  useEffect(() => {
+    connection();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <StatusBar />
+      <FlatList
+        data={users}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Text>{item.name}</Text>}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
